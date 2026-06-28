56a305a7accddb7922fd613668ee19abe4a6454e
PB101-cpu <tubai.bagchi@gmail.com>
2026-06-28 21:32:53 +0530

    Update streamlit_app.py
    sasad

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,14 +61,14 @@ if ingredients_list:
         sf_df = st.dataframe(
             data=smoothiefroot_response.json(), use_container_width=True
         )
-##my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     ##values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-##time_to_insert=st.button('Submit Order')
+time_to_insert=st.button('Submit Order')
 #st.write(my_insert_stmt)
 
-##if time_to_insert:
-   ## session.sql(my_insert_stmt).collect()
-    ##st.success('Your Smoothie is ordered!', icon="✅")
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered!', icon="✅")
 
 
 #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)  
